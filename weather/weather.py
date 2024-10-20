0439a6d285e996075ea881251129a34e4c3e3077
XATIXATIX <xatixatix@gmail.com>
2024-10-20 21:03:49 +0200

    remove print

--- weather/weather.py
+++ weather/weather.py
@@ -3,7 +3,6 @@ import requests
 async def get_weather_data(location, api_key):
     response = requests.get(f'https://api.weatherapi.com/v1/current.json?key={api_key}&q={location}&aqi=no')
     data = response.json()
-    print(f'{data}')
     return weather_to_string(data)
 
 def weather_to_string(weather_data):
